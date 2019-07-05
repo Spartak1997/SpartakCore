cacefb739807f313ab678527cad23b9ab7c8b8c8
Спартак <spartak_korolev@mail.ru>
2019-07-05 09:13:44 +0300

    Try #2

--- src/main/java/com/SpartakCore/SpartakCore.java
+++ src/main/java/com/SpartakCore/SpartakCore.java
@@ -1,5 +1,6 @@
 package com.SpartakCore;
 
+import com.SpartakCore.command.*;
 import com.SpartakCore.config.CoreModConfig;
 import com.SpartakCore.creativetab.ModTabList;
 import com.SpartakCore.galacticgreg.SpaceDimRegisterer;
@@ -79,7 +80,7 @@ public class SpartakCore {
         if (!CoreConfig.LoadConfig()) {
             Logger.error(String.format("%s could not load its config file. Things are going to be weird!", Refstrings.MODID));
         }
-        // ------------------------------------------------------------
+     // ------------------------------------------------------------
 
         if (CoreConfig.ModAdminErrorLogs_Enabled)
         {
@@ -87,8 +88,28 @@ public class SpartakCore {
             Module_AdminErrorLogs = new IngameErrorLog();
         }
         
-        proxy.addTexturePage();
+     // ------------------------------------------------------------
+        Logger.debug("PRELOAD Init TexturePage");
+        File tFile = new File(new File(PreEvent.getModConfigurationDirectory(), "GregTech"), "GregTech.cfg");
+        Configuration tMainConfig = new Configuration(tFile);
+        tMainConfig.load();
         
+        /*GregTech_API.sUseMachineMetal = tMainConfig.get("machines", "use_machine_metal_tint", true).getBoolean(true);
+        if (GregTech_API.sUseMachineMetal)
+            {
+                // use default in GregTech Dyes enum.
+            }
+        else
+            {
+                // Override MACHINE_METAL dye color with white
+                MACHINE_METAL.mRGBa[0]= 255;
+                MACHINE_METAL.mRGBa[1]= 255;
+                MACHINE_METAL.mRGBa[2]= 255;
+            }*/
+
+        proxy.addTexturePage();
+     // ------------------------------------------------------------
+
      // ------------------------------------------------------------
         Logger.debug("PRELOAD Init itemmanager");
         ItemManager = new ModItemManager(Refstrings.MODID);
@@ -148,6 +169,8 @@ public class SpartakCore {
 	@Mod.EventHandler
     public void serverLoad(FMLServerStartingEvent pEvent)
     {
-		
+		if (CoreConfig.ModItemInHandInfo_Enabled) {
+            pEvent.registerServerCommand(new ItemInHandInfoCommand());
+        }
     }
 }
--- src/main/java/com/SpartakCore/config/CoreModConfig.java
+++ src/main/java/com/SpartakCore/config/CoreModConfig.java
@@ -14,16 +14,19 @@ public class CoreModConfig extends ConfigManager{
 	  }
 	
 	public boolean ModAdminErrorLogs_Enabled;
+	public boolean ModItemInHandInfo_Enabled;
 	
 	@Override
 	  protected void PreInit()
 	  {
 		ModAdminErrorLogs_Enabled = true;
+		ModItemInHandInfo_Enabled = false;
 	  }
 
 	@Override
 	  protected void Init()
 	  {
+		ModItemInHandInfo_Enabled = _mainConfig.getBoolean( "ItemInHandInfo", "Modules", ModItemInHandInfo_Enabled, "Set to true to enable ItemInHandInfo module. If enabled, type /iih to display the item's name-info" );
 		ModAdminErrorLogs_Enabled = _mainConfig.getBoolean( "AdminErrorLog", "Modules", ModAdminErrorLogs_Enabled, "If set to true, every op/admin will receive all errors occoured during the startup phase as ingame message on join" );
 	  }
 	
