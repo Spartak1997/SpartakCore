0755308f1a438c55ddfb4580341075fd6ed368d8
Спартак <spartak_korolev@mail.ru>
2019-07-29 21:54:28 +0300

    Add recipes from TecTech

--- src/main/java/com/spartakcore/gthandler/GT_CustomLoader.java
+++ src/main/java/com/spartakcore/gthandler/GT_CustomLoader.java
@@ -13,10 +13,16 @@ public class GT_CustomLoader {
 
 	private static final GT_Loader_Items ItemLoader = new GT_Loader_Items();
 	private static final GT_MachineRecipeLoader MachineRecipeLoader = new GT_MachineRecipeLoader();
+	private static final GT_TecTechLoader TecTechLoader = new GT_TecTechLoader();
+	
 	
 	public static void run()
     {
 		ItemLoader.run();
 		MachineRecipeLoader.run();
+		if (Loader.isModLoaded("tectech"))
+	    {
+		TecTechLoader.run();
+	    }
     }
 }
