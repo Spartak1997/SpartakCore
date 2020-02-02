b89b1c76b3e37164ff6cdc93d66fe4fe4bb66a83
Спартак <spartak_korolev@mail.ru>
2020-02-02 22:01:59 +0600

    Delete ULV CircAss

--- src/main/java/com/spartakcore/gthandler/GT_Loader_Machines.java
+++ src/main/java/com/spartakcore/gthandler/GT_Loader_Machines.java
@@ -330,13 +330,13 @@ public class GT_Loader_Machines {
 					    	'C', OrePrefixes.circuit.get(Materials.Advanced),
 					    	'W', OrePrefixes.cableGt01.get(Materials.Gold),
 					    	'G', OrePrefixes.gearGt.get(Materials.StainlessSteel)});
+				
 				// ===================================================================================================
-				// Primitive Assemblers
+				// Primitive Assembler
 				// ===================================================================================================
 
 				CustomItemList.Machine_ULV_Assembler.set(new GT_MetaTileEntity_BasicMachine_GT_Recipe(13032, "basicmachine.assembler.tier.00", "Primitive Assembler", 0, "Avengers, Assemble!", GT_Recipe.GT_Recipe_Map.sAssemblerRecipes, 3, 1, 8000, 0, 1, "Assembler0.png", GregTech_API.sSoundList.get(204), false, false, 0, "ASSEMBLER", null).getStackForm(1L));
-				//CustomItemList.Machine_ULV_CircuitAssembler.set(new GT_MetaTileEntity_BasicMachine_GT_Recipe( 13033, "basicmachine.circuitassembler.tier.00", "Primitive Circuit Assembler", 0, "Pick-n-Place all over the place", GT_Recipe.GT_Recipe_Map.sCircuitAssemblerRecipes, 6, 1, 8000, 0, 1, "CircuitAssembler.png", GregTech_API.sSoundList.get(204), false, false, 0, "CIRCUITASSEMBLER", null).getStackForm(1L));
-
+				
 				GT_ModHandler.addCraftingRecipe(CustomItemList.Machine_ULV_Assembler.get(1L),
 				bitsd,
 				new Object[]{"RCR", "OMO", "WCW",
